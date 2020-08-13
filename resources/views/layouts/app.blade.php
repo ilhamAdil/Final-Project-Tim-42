66c3d83cbf74e1d018767801d4e3fda4beb3cadb
Arif Hidayat <sharerifdotcom@gmail.com>
2020-08-13 20:45:45 +0700

    tambah container di app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,8 +56,8 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -73,7 +73,9 @@
         </nav>
 
         <main class="py-4">
-            @yield('content')
+        <div class="container">
+        @yield('content')
+        </div>
         </main>
     </div>
 </body>
