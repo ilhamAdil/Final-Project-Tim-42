176e122d63b9265a561ea84299a59b8bb4b271df
Redmist-bit <bayuadeirawan79@gmail.com>
2020-08-14 09:57:34 +0800

    add route & controller resource answer

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('questions', 'QuestionsController');
+Route::resource('answers', 'AnswersController');
