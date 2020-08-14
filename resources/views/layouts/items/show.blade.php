8b141202a80fdab95734b086d8d02b8b1500654b
Redmist-bit <bayuadeirawan79@gmail.com>
2020-08-15 04:16:06 +0800

    tambah form

--- resources/views/layouts/items/show.blade.php
+++ resources/views/layouts/items/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="card">
+<div class="card mb-2">
     <div class="card-header">
         Pertanyaan #{{$question->id}}
         <div class="d-flex flex-row-reverse bd-highlight float-right">
@@ -24,10 +24,19 @@
             @endforelse
 
             <div class="float-right">
+                <button class="btn btn-success btn-sm mr-3">Comments</button>
                 <a href="#" class="btn btn-outline-primary btn-sm m-1"><i class="fas fa-thumbs-up"></i></a>
                 <a href="#" class="btn btn-outline-danger btn-sm m-1"><i class="fas fa-thumbs-down"></i></a>
             </div>
         </div>
+        
     </div>
 </div>
+<label for="isi" class="mt-4"><strong>Tambahkan Jawaban</strong></label>
+<form action="" class="form-group mt-3">
+    <textarea name="body" id="body" class="form-control" cols="30" rows="10" placeholder="Masukan jawaban anda"></textarea>
+    <button type="submit" class="btn btn-primary mt-2">Submit</button>
+</form>
+
+
 @endsection
